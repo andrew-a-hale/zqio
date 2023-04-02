a95cc2765ab9fc32108a45d1123effe54848e2f4
Andrew Hale <andrew.hale003@gmail.com>
2023-04-02 17:03:07 +1000

    added benchmarking, dijkstras, and path to recursive solver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from puzzle import puzzle
 import logging
+import timeit
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(levelname)s - %(message)s",
@@ -7,12 +8,23 @@ logging.basicConfig(level=logging.INFO,
 
 
 def main():
-    p = puzzle.Puzzle(10, 4)
+    p = puzzle.Puzzle(50, 1)
+    p.print_grid()
+    p.solve_grid_dijkstra()
+    p.__repr__()
     p.solve_grid_recursive()
     p.__repr__()
     p.solve_grid_brute_force()
     p.__repr__()
 
+def benchmark():
+    call = "puzzle.Puzzle(50, 1)"
+    return {
+        "dijkstra": timeit.timeit("p.solve_grid_dijkstra()", setup=f"from puzzle import puzzle; p = {call}", number=1),
+        "recursive": timeit.timeit("p.solve_grid_recursive()", setup=f"from puzzle import puzzle; p = {call}", number=1),
+        "brute_force": timeit.timeit("p.solve_grid_brute_force()", setup=f"from puzzle import puzzle; p = {call}", number=1)
+    }
 
 if __name__ == "__main__":
     main()
+    print(benchmark())
--- puzzle/puzzle.py
+++ puzzle/puzzle.py
@@ -2,6 +2,7 @@ import itertools
 import logging
 import math
 import random
+from typing import List, Tuple
 
 
 class Puzzle():
@@ -11,9 +12,8 @@ class Puzzle():
         self.path_height = grid_size - 1
         self.path_size = self.path_height * 2
         self.seed = seed
-
-        logging.info(
-            f"Searching {math.comb(self.path_size, self.path_height)} paths")
+        self.path = None
+        self.score = None
 
         random.seed(seed)
         self.grid = [
@@ -22,6 +22,16 @@ class Puzzle():
 
     def solve_grid_brute_force(self):
         """solve by calculating all paths and selecting the maximum"""
+        self.solver_type = "BRUTE FORCE"
+                
+        logging.info(
+            f"Searching {math.comb(self.path_size, self.path_height)} paths")
+
+        if self.path_size > 22:
+            logging.warning(
+                "Brute force is too slow for this size, returning None")
+            return None
+
         path_stubs = itertools.combinations(list(range(self.path_size)),
                                             self.path_height)
 
@@ -37,25 +47,91 @@ class Puzzle():
         scores = [sum(self.grid[x] for x in path) for path in paths]
         best_path = max(range(len(scores)), key=scores.__getitem__)
         self.score = scores[best_path]
-        self.path = paths[best_path]
+        self.path = self.format_path(paths[best_path])
 
     def solve_grid_recursive(self):
         """solve by recursively calculating the best path"""
-        memo = [[0 for _ in range(self.grid_size)]
+        self.solver_type = "RECURSIVE"
+
+        # set up memoisation and grid
+        memo = [[(0, []) for _ in range(self.grid_size)]
                 for _ in range(self.grid_size)]
         grid_2d = self._flat_to_2d()
 
-        def helper(i, j) -> int:
+        # recursive function to calculate the best path
+        def helper(i, j) -> Tuple[int, List[Tuple[int, int]]]:
+            if memo[i][j][0] > 0:
+                return memo[i][j]
+            if i == 0 and j == 0:
+                memo[i][j] = (grid_2d[i][j], [(i, j)])
+                return memo[i][j]
             max_sum = -1
-            if i == 0 and j == 0: return grid_2d[i][j]
-            if memo[i][j] > 0: return memo[i][j]
-            if j > 0: max_sum = max(max_sum, grid_2d[i][j] + helper(i, j - 1))
-            if i > 0: max_sum = max(max_sum, grid_2d[i][j] + helper(i - 1, j))
-            memo[i][j] = max_sum
-            return max_sum
+            max_path = []
+            if i > 0:
+                sum_right, path_right = helper(i - 1, j)
+                if sum_right > max_sum:
+                    max_sum = sum_right
+                    max_path = path_right
+            if j > 0:
+                sum_up, path_up = helper(i, j - 1)
+                if sum_up > max_sum:
+                    max_sum = sum_up
+                    max_path = path_up
+            max_path = max_path.copy()
+            
+            # coordinates are reversed because the grid is flipped in the recursive solution
+            max_path.append((j, i))
+            memo[i][j] = (max_sum + grid_2d[i][j], max_path)
+            return memo[i][j]
 
-        self.score = helper(self.grid_size - 1, self.grid_size - 1)
-        self.path = None
+        self.score, self.path = helper(self.grid_size - 1, self.grid_size - 1)
+
+    def solve_grid_dijkstra(self):
+        """solve by using dijkstra's algorithm"""
+        self.solver_type = "DIJKSTRAS"
+        
+        # create a graph of the grid
+        nodes = [x for x in range(self.grid_size**2)]
+        edges = ([(x, x + 1)
+                  for x in nodes if x % self.grid_size != self.grid_size - 1] +
+                 [(x, x + self.grid_size)
+                  for x in nodes if x < self.grid_size**2 - self.grid_size])
+        max_dist = max(self.grid)
+        dist = [max_dist - x for x in self.grid]
+
+        # dijkstra's algorithm initialisation
+        target = nodes[-1]
+        unvisited = set(nodes)
+        path = []
+        path_cost = {i: float('inf') for i, _ in enumerate(unvisited)}
+        path_cost[0] = 0
+
+        # find the shortest path using dijkstra's algorithm from source to target
+        while unvisited:
+            current = min(unvisited, key=path_cost.__getitem__)
+            unvisited.remove(current)
+            if current == target:
+                break
+            for edge in edges:
+                if current == edge[0]:
+                    neighbour = edge[1]
+                    if neighbour in unvisited:
+                        new_cost = path_cost[current] + dist[neighbour]
+                        if new_cost < path_cost[neighbour]:
+                            path_cost[neighbour] = new_cost
+                            path.append((current, neighbour))
+
+        # extract the shortest path from the path list
+        shortest_path = []
+        for edge in reversed(path):
+            if len(shortest_path) == 0 and edge[1] == target:
+                shortest_path.append(edge)
+            if len(shortest_path) > 0 and edge[1] == shortest_path[-1][0]:
+                shortest_path.append(edge)
+
+        self.path = [0] + [x[1] for x in reversed(shortest_path)]
+        self.score = sum(self.grid[x] for x in self.path)
+        self.path = self.format_path(self.path)
 
     def _flat_to_2d(self) -> list:
         return [
@@ -63,10 +139,22 @@ class Puzzle():
             for x in range(self.grid_size)
         ]
 
-    def __repr__(self):
+    def print_grid(self):
         grid_2d = self._flat_to_2d()
-        [print(x) for x in reversed(grid_2d)]
-        print(self.score)
+        width = math.ceil(math.log10(self.grid_size))
+        for row in reversed(grid_2d):
+            for col in row:
+                print(f"{col:>{width}}", end=" ")
+            print("")
+        print("")
+
+    def format_path(self, path) -> List[Tuple[int, int]]:
+        return [(x % self.grid_size, x // self.grid_size) for x in path]
+
+    def __repr__(self):
+        print(self.solver_type)
+        if self.score:
+            print(self.score)
         if self.path:
-            print([(x % self.grid_size, x // self.grid_size)
-                   for x in self.path])
+            print(self.path)
+        print()
